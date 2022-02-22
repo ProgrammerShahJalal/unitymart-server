2b0c10b8b50017973cd42236de0552eb2ab4c1e8
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-02-22 19:40:10 +0600

    payment gateway

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
 const app = express();
 const cors = require('cors');
+const SSLCommerzPayment = require('sslcommerz');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -14,6 +15,58 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tllgu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+// payment initialize api
+app.get('/init', (req, res) => {
+    const data = {
+        total_amount: 100,
+        currency: 'EUR',
+        tran_id: 'REF123',
+        success_url: 'https://morning-inlet-49130.herokuapp.com/success',
+        fail_url: 'https://morning-inlet-49130.herokuapp.com/fail',
+        cancel_url: 'https://morning-inlet-49130.herokuapp.com/cancel',
+        ipn_url: 'http://yoursite.com/ipn',
+        shipping_method: 'Courier',
+        product_name: 'Computer.',
+        product_category: 'Electronic',
+        product_profile: 'general',
+        cus_name: 'Customer Name',
+        cus_email: 'cust@yahoo.com',
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+        multi_card_name: 'mastercard',
+        value_a: 'ref001_A',
+        value_b: 'ref002_B',
+        value_c: 'ref003_C',
+        value_d: 'ref004_D'
+    };
+    const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASSWORD, false) //true for live default false for sandbox
+    sslcommer.init(data).then(data => {
+        //process the response that got from sslcommerz
+        //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+        res.redirect(data.GatewayPageURL)
+    });
+})
+
+app.post('/success', async (req, res) => {
+    console.log(req.body);
+    res.status(200).json(req.body)
+})
+
+
 
 async function verifyToken(req, res, next) {
     if (req.headers?.authorization?.startsWith('Bearer ')) {
