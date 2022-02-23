f037af50e939168066735bc9b8348f4259596bc3
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-02-23 18:05:18 +0600

    data loaded

--- index.js
+++ index.js
@@ -35,6 +35,83 @@ async function verifyToken(req, res, next) {
     next();
 }
 
+// payment initialize api
+app.post('/init', async (req, res) => {
+    const data = {
+        total_amount: req.body.total_amount,
+        currency: 'BDT',
+        tran_id: uuidv4(),
+        success_url: 'https://morning-inlet-49130.herokuapp.com/success',
+        fail_url: 'https://morning-inlet-49130.herokuapp.com/fail',
+        cancel_url: 'https://morning-inlet-49130.herokuapp.com/cancel',
+        ipn_url: 'https://morning-inlet-49130.herokuapp.com/ipn',
+        paymentStatus: 'pending',
+        shipping_method: 'Courier',
+        product_name: req.body.product_name,
+        product_category: 'Electronic',
+        product_image: req.body.product_image,
+        product_profile: req.body.product_profile,
+        cus_name: req.body.cus_name,
+        cus_email: req.body.cus_email,
+        cus_add1: 'Dhaka',
+        cus_add2: 'Dhaka',
+        cus_city: 'Dhaka',
+        cus_state: 'Dhaka',
+        cus_postcode: '1000',
+        cus_country: 'Bangladesh',
+        cus_phone: '01711111111',
+        cus_fax: '01711111111',
+        ship_name: 'Customer Name',
+        ship_add1: 'Dhaka',
+        ship_add2: 'Dhaka',
+        ship_city: 'Dhaka',
+        ship_state: 'Dhaka',
+        ship_postcode: 1000,
+        ship_country: 'Bangladesh',
+        multi_card_name: 'mastercard',
+        value_a: 'ref001_A',
+        value_b: 'ref002_B',
+        value_c: 'ref003_C',
+        value_d: 'ref004_D'
+    };
+    const order = await ordersCollection.insertOne(data);
+    const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASSWORD, false) //true for live default false for sandbox
+    sslcommer.init(data).then(data => {
+        //process the response that got from sslcommerz
+        //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+
+        if (data.GatewayPageURL) {
+            res.json(data.GatewayPageURL)
+        }
+        else {
+            return res.status(400).json({
+                message: "SSL session was not successful"
+            })
+        }
+    });
+})
+
+
+app.post('/success', async (req, res) => {
+    const order = await ordersCollection.updateOne({ tran_id: req.body.tran_id }, {
+        $set: {
+            val_id: req.body.val_id
+        }
+    })
+
+    res.redirect(`https://unitymart-c522a.web.app/success/${req.body.tran_id}`)
+})
+app.post('/fail', async (req, res) => {
+    res.status(400).redirect(`https://unitymart-c522a.web.app`)
+    const order = await ordersCollection.deleteOne({ tran_id: req.body.tran_id })
+})
+app.post('/cancel', async (req, res) => {
+    res.status(200).redirect(`https://unitymart-c522a.web.app`)
+})
+const order = await ordersCollection.deleteOne({ tran_id: req.body.tran_id })
+
+/* -----------------------ssl commerce completed------------- */
+
 async function run() {
     try {
         await client.connect();
@@ -49,82 +126,6 @@ async function run() {
         const featuresCollection = database.collection('features');
         const specialsCollection = database.collection('specials');
 
-        // payment initialize api
-        app.post('/init', async (req, res) => {
-            const data = {
-                total_amount: req.body.total_amount,
-                currency: 'BDT',
-                tran_id: uuidv4(),
-                success_url: 'https://morning-inlet-49130.herokuapp.com/success',
-                fail_url: 'https://morning-inlet-49130.herokuapp.com/fail',
-                cancel_url: 'https://morning-inlet-49130.herokuapp.com/cancel',
-                ipn_url: 'https://morning-inlet-49130.herokuapp.com/ipn',
-                paymentStatus: 'pending',
-                shipping_method: 'Courier',
-                product_name: req.body.product_name,
-                product_category: 'Electronic',
-                product_image: req.body.product_image,
-                product_profile: req.body.product_profile,
-                cus_name: req.body.cus_name,
-                cus_email: req.body.cus_email,
-                cus_add1: 'Dhaka',
-                cus_add2: 'Dhaka',
-                cus_city: 'Dhaka',
-                cus_state: 'Dhaka',
-                cus_postcode: '1000',
-                cus_country: 'Bangladesh',
-                cus_phone: '01711111111',
-                cus_fax: '01711111111',
-                ship_name: 'Customer Name',
-                ship_add1: 'Dhaka',
-                ship_add2: 'Dhaka',
-                ship_city: 'Dhaka',
-                ship_state: 'Dhaka',
-                ship_postcode: 1000,
-                ship_country: 'Bangladesh',
-                multi_card_name: 'mastercard',
-                value_a: 'ref001_A',
-                value_b: 'ref002_B',
-                value_c: 'ref003_C',
-                value_d: 'ref004_D'
-            };
-            const order = await ordersCollection.insertOne(data);
-            const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASSWORD, false) //true for live default false for sandbox
-            sslcommer.init(data).then(data => {
-                //process the response that got from sslcommerz
-                //https://developer.sslcommerz.com/doc/v4/#returned-parameters
-
-                if (data.GatewayPageURL) {
-                    res.json(data.GatewayPageURL)
-                }
-                else {
-                    return res.status(400).json({
-                        message: "SSL session was not successful"
-                    })
-                }
-            });
-        })
-
-
-        app.post('/success', async (req, res) => {
-            const order = await ordersCollection.updateOne({ tran_id: req.body.tran_id }, {
-                $set: {
-                    val_id: req.body.val_id
-                }
-            })
-
-            res.redirect(`https://unitymart-c522a.web.app/success/${req.body.tran_id}`)
-        })
-        app.post('/fail', async (req, res) => {
-            res.status(400).redirect(`https://unitymart-c522a.web.app`)
-            const order = await ordersCollection.deleteOne({ tran_id: req.body.tran_id })
-        })
-        app.post('/cancel', async (req, res) => {
-            res.status(200).redirect(`https://unitymart-c522a.web.app`)
-        })
-        const order = await ordersCollection.deleteOne({ tran_id: req.body.tran_id })
-
-        /* -----------------------ssl commerce completed------------- */
 
         /* -------------BLOGS COLLECTION------------ */
         // GET BLOGS API
