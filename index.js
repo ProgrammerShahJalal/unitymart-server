983e7ce16cafdd805ae0a178b9971046b3f334b4
Md Shah Jalal <bestwebdesignerr@gmail.com>
2022-02-25 01:54:32 +0600

    payment gateway completed

--- index.js
+++ index.js
@@ -18,6 +18,130 @@ app.use(express.urlencoded({ extended: true }));
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tllgu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
+client.connect(err => {
+    // Create collection
+    const orderCollection = client.db("paymentssl").collection("orders");
+
+    // Initialize payment
+    app.post('/init', async (req, res) => {
+        console.log("hitting")
+        const productInfo = {
+            total_amount: req.body.total_amount,
+            currency: 'BDT',
+            tran_id: uuidv4(),
+            success_url: 'https://morning-inlet-49130.herokuapp.com/success',
+            fail_url: 'https://morning-inlet-49130.herokuapp.com/failure',
+            cancel_url: 'https://morning-inlet-49130.herokuapp.com/cancel',
+            ipn_url: 'https://morning-inlet-49130.herokuapp.com/ipn',
+            paymentStatus: 'pending',
+            shipping_method: 'Courier',
+            product_name: req.body.product_name,
+            product_category: 'Electronic',
+            product_profile: req.body.product_profile,
+            product_image: req.body.product_image,
+            cus_name: req.body.cus_name,
+            cus_email: req.body.cus_email,
+            cus_add1: 'Dhaka',
+            cus_add2: 'Dhaka',
+            cus_city: 'Dhaka',
+            cus_state: 'Dhaka',
+            cus_postcode: '1000',
+            cus_country: 'Bangladesh',
+            cus_phone: '01711111111',
+            cus_fax: '01711111111',
+            ship_name: req.body.cus_name,
+            ship_add1: 'Dhaka',
+            ship_add2: 'Dhaka',
+            ship_city: 'Dhaka',
+            ship_state: 'Dhaka',
+            ship_postcode: 1000,
+            ship_country: 'Bangladesh',
+            multi_card_name: 'mastercard',
+            value_a: 'ref001_A',
+            value_b: 'ref002_B',
+            value_c: 'ref003_C',
+            value_d: 'ref004_D'
+        };
+
+        // Insert order info
+        const result = await orderCollection.insertOne(productInfo);
+
+        const sslcommer = new SSLCommerzPayment(process.env.STORE_ID, process.env.STORE_PASSWORD, false) //true for live default false for sandbox
+        sslcommer.init(productInfo).then(data => {
+            //process the response that got from sslcommerz 
+            //https://developer.sslcommerz.com/doc/v4/#returned-parameters
+            const info = { ...productInfo, ...data }
+            // console.log(info.GatewayPageURL);
+            if (info.GatewayPageURL) {
+                res.json(info.GatewayPageURL)
+            }
+            else {
+                return res.status(400).json({
+                    message: "SSL session was not successful"
+                })
+            }
+
+        });
+    });
+    app.post("/success", async (req, res) => {
+
+        const result = await orderCollection.updateOne({ tran_id: req.body.tran_id }, {
+            $set: {
+                val_id: req.body.val_id
+            }
+        })
+
+        res.redirect(`https://unitymart-c522a.web.app/success/${req.body.tran_id}`)
+
+    })
+    app.post("/failure", async (req, res) => {
+        const result = await orderCollection.deleteOne({ tran_id: req.body.tran_id })
+
+        res.redirect(`https://unitymart-c522a.web.app`)
+    })
+    app.post("/cancel", async (req, res) => {
+        const result = await orderCollection.deleteOne({ tran_id: req.body.tran_id })
+
+        res.redirect(`https://unitymart-c522a.web.app`)
+    })
+    app.post("/ipn", (req, res) => {
+        console.log(req.body)
+        res.send(req.body);
+    })
+    app.post('/validate', async (req, res) => {
+        const result = await orderCollection.findOne({
+            tran_id: req.body.tran_id
+        })
+
+        if (result.val_id === req.body.val_id) {
+            const update = await orderCollection.updateOne({ tran_id: req.body.tran_id }, {
+                $set: {
+                    paymentStatus: 'paymentComplete'
+                }
+            })
+            console.log(update);
+            res.send(update.modifiedCount > 0)
+
+        }
+        else {
+            res.send("Eaa Boro Chor detected")
+        }
+
+    })
+    // ORDER ACCORDING TO TRAIN_ID
+    app.get('/orders/:tran_id', async (req, res) => {
+        const id = req.params.tran_id;
+        const result = await orderCollection.findOne({ tran_id: id })
+        res.json(result)
+    })
+    // ALL ORDERS
+    app.get("/orders", async (req, res) => {
+        const result = await orderCollection.find({}).toArray();
+        res.send(result);
+        console.log(result);
+    });
+
+});
 
 async function verifyToken(req, res, next) {
     if (req.headers?.authorization?.startsWith('Bearer ')) {
@@ -34,6 +158,7 @@ async function verifyToken(req, res, next) {
     }
     next();
 }
+
 
 
 async function run() {
